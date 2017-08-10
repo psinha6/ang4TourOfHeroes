efcc68c26541af823953c8178a34754c8f94a809
Prateek <psinha6@gmail.com>
2017-08-10 06:24:02 +0530

    added search part 1

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http,Headers } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 
@@ -9,7 +9,7 @@ import { HEROES } from './mock-heroes';
 
 export class HeroService {
     private heroesUrl = 'api/heroes';  // URL to web api
-
+    private headers = new Headers({'Content-Type': 'application/json'});    
     constructor(private http: Http) { }
 
     getHeroes(): Promise<Hero[]> {
@@ -23,8 +23,39 @@ export class HeroService {
         console.error('An error occured' + error);
         return Promise.reject(error.message || error);
     }
-    getHero(id: number): Promise<Hero> {
+    /*getHero(id: number): Promise<Hero> {
         return this.getHeroes()
         .then(heroes => heroes.find(hero => hero.id === id));
+    }*/
+    getHero(id: number): Promise<Hero> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.get(url)
+            .toPromise()
+            .then(response => response.json().data as Hero)
+            .catch(this.handleError);
+    }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), {headers: this.headers})
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }
+    create(name: string): Promise<Hero>{
+        return this.http
+        .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+        .toPromise()
+        .then(res => res.json().data as Hero)
+        .catch(this.handleError);
+    }
+    
+    delete(id: number): Promise<void> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.delete(url, {headers: this.headers})
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
     }
 }
